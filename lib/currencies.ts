8531d8960593282cb1d41071104f762642fe9d84
kaMat Prahlad <kamatprahlad43@gmail.com>
2024-05-03 03:38:08 +0530

    git init

--- lib/currencies.ts
+++ lib/currencies.ts
@@ -3,6 +3,7 @@ export const Currencies = [
   { value: "EUR", label: "€ Euro", locale: "de-DE" },
   { value: "JPY", label: "¥ Yen", locale: "ja-JP" },
   { value: "GBP", label: "£ Pound", locale: "en-GB" },
+  { value: "IND", label: "₹ Rupess", locale: "en-IN"},
 ];
 
 export type Currency = (typeof Currencies)[0];
