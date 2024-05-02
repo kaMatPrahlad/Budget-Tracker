8152cf0b9c249d9651378c810a4b702b661c9f92
kaMat Prahlad <kamatprahlad43@gmail.com>
2024-05-03 03:41:36 +0530

    indian currencies added

--- lib/currencies.ts
+++ lib/currencies.ts
@@ -3,7 +3,7 @@ export const Currencies = [
   { value: "EUR", label: "€ Euro", locale: "de-DE" },
   { value: "JPY", label: "¥ Yen", locale: "ja-JP" },
   { value: "GBP", label: "£ Pound", locale: "en-GB" },
-  { value: "IND", label: "₹ Rupess", locale: "en-IN"},
+  { value: "INR", label: "₹ Rupee", locale: "en-IN" },
 ];
 
 export type Currency = (typeof Currencies)[0];
